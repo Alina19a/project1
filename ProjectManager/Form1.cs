5994def14c4f2d46e15a7f2b1ba171fecbfef465
Alina19a <nartovaalina515@gmail.com>
2025-01-30 08:03:04 +0300

    Initial commit

---
+++ ProjectManager/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Запускаем ProjectOne как отдельный процесс
+            System.Diagnostics.Process.Start("ProjectOne.exe");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+			ProjectTwo.Form1 form1 = new ProjectTwo.Form1();
+			form1.Show();
+		}
+    }
+}
---
+++ ProjectOne/Calendar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Windows.Forms;
+
+namespace ProjectOne
+{
+    internal class Calendar
+    {
+        [STAThread] // Это необходимо для работы с компонентами Windows Forms
+        public static void Main()
+        {
+            DateTime now = GetCurrentDate();
+           MessageBox.Show($"Today's date is {now}")
+        }
+
+        static DateTime GetCurrentDate()
+        {
+            return DateTime.Now.Date;
+        }
+    }
+}
